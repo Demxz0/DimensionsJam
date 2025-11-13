d28bfe3ec926772c0904b8643f90dfd014e5e095
zaid-8 <alhaderzaed65@gmail.com>
2025-11-13 17:39:41 +0300

    init zaid

---
+++ diagamejam/zaid/main_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+#called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+	
+	#Called every frame. 'delta' is the elapsed time since the previous fra
+func _process(delta):
+		pass
+
+
+func _on_start_pressed():
+	print("Start pressed")
+
+
+func _on_settings_pressed():
+	print("Settings pressed")
+
+
+func _on_Exit_pressed():
+	print("Exit pressed")
