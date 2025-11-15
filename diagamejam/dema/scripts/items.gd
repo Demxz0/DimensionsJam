ca09e8e8570fdb03cdc3ec93b52fba2d66eb30e0
Dema <demaaljundi5@gmail.com>
2025-11-14 19:52:20 -0800

    error fixed

--- diagamejam/dema/scripts/items.gd
+++ diagamejam/dema/scripts/items.gd
@@ -1,11 +1,10 @@
 extends Node2D 
 
-
 @onready var option_button = $"../Node2D5/CheckBox3"
 @onready var report_button = $"../Node2D5/Button"
 
 func _ready():
-	
+
 	report_button.pressed.connect(_on_report_button_pressed)
 
 func _on_report_button_pressed():
@@ -14,10 +13,8 @@ func _on_report_button_pressed():
 	if selected_index == -1:
 		print("Please select a character first!")
 		return
-
 	
 	var selected_character = option_button.get_item_text(selected_index)
-
 
 	match selected_character:
 		"Amina":
